b59979f312c50447de716a8708dbed7244893f59
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-17 19:42:53 +0500

    Create users.py, create functions for joining

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from all_json import SETTINGS
+import users
 
 bot = telebot.TeleBot(SETTINGS["telegram_api_token"], parse_mode=None)
 
@@ -12,6 +13,18 @@ def send_welcome_in_not_group(message):
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     bot.reply_to(message, "Hey")
+
+
+@bot.message_handler(commands=["join"])
+def join_user(message):
+    users.join_user(message.from_user.id, message.from_user.username)
+    bot.reply_to(message, f"User @{message.from_user.username} was joined")
+
+
+@bot.message_handler(commands=["leave"])
+def leave_user(message):
+    users.leave_user(message.from_user.id, message.from_user.username)
+    bot.reply_to(message, f"User @{message.from_user.username} was leaved")
 
 
 bot.infinity_polling()
---
+++ users.py
@@ -0,0 +1,51 @@
+from spreadsheet import SHEET
+
+WORKSHEET = SHEET.worksheet("Users")
+
+
+def get_info_about_user(user_id=None, username=None):
+    if user_id:
+        find_index = 0
+        find_thing = str(user_id)
+    elif username:
+        find_index = 1
+        find_thing = str(username)
+    else:
+        return Exception("You have to pass user_id or username")
+
+    all_values = WORKSHEET.get_all_values()
+
+    for i in range(len(all_values)):
+        if all_values[i][find_index] == find_thing:
+            info = [int(all_values[i][0]), all_values[i][1], int(all_values[i][2]), int(all_values[i][3])]
+            return i + 1, info
+
+    return None
+
+
+def add_user(user_id, username, joined):
+    all_values = WORKSHEET.get_all_values()
+    new_line_n = len(all_values) + 1
+
+    WORKSHEET.update(f"A{new_line_n}:Z{new_line_n}", [[user_id, username, 0, joined]])
+
+
+def edit_joined(user_id, username, joined):
+    info_about_user = get_info_about_user(user_id)
+
+    if info_about_user:
+        row, info = info_about_user
+
+        if info[3] == joined and joined == 1:
+            return "User @{} already joined"
+        elif info[3] == joined and joined == 0:
+            return "User @{} already left"
+    else:
+        add_user(user_id, username, joined)
+
+
+
+
+
+def update_balances_after_payment(user_id, amount):
+    pass
