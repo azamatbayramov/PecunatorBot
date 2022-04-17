f488852c886b58c84c53c26e6de9a25039f1bc10
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-17 18:12:15 +0500

    Create functions for spreadsheet setup

--- all_json.py
+++ all_json.py
@@ -4,4 +4,4 @@ import json
 
 SETTINGS = json.load(open("json/settings.json", encoding="utf8"))
 CREDENTIALS_FILENAME = "json/credentials.json"
-
+WORKSHEET_SETTINGS = json.load(open("json/worksheet_settings.json", encoding="utf8"))
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,22 +1,33 @@
 import gspread
-from all_json import SETTINGS, CREDENTIALS_FILENAME
+from all_json import SETTINGS, CREDENTIALS_FILENAME, WORKSHEET_SETTINGS as WS_S
 
-gc = gspread.service_account(CREDENTIALS_FILENAME)
+gc = gspread.service_account(filename=CREDENTIALS_FILENAME)
 
 sh = gc.open_by_url(SETTINGS["google_spreadsheet_url"])
 
-necessary_worksheets = [
-    ("Payments", 100, 10),
-    ("Users", 100, 10),
-    ("Resets", 100, 10)
-]
-
 
 def create_necessary_worksheets(sheet: gspread.Spreadsheet):
-    for worksheet in necessary_worksheets:
-        sheet.add_worksheet(*worksheet)
+    existing_worksheets = list(map(lambda s: s.title, sheet.worksheets()))
+    for worksheet_name in WS_S["necessary_worksheets"]:
+        if worksheet_name not in existing_worksheets:
+            sheet.add_worksheet(worksheet_name, WS_S["rows_count"], WS_S["columns_count"])
 
 
-def delete_all_worksheets(sheet: gspread.Spreadsheet):
+def clear_all_worksheets(sheet: gspread.Spreadsheet):
     for worksheet in sheet.worksheets():
-        sheet.del_worksheet(worksheet)
+        worksheet.clear()
+
+
+def delete_unnecessary_worksheets(sheet: gspread.Spreadsheet):
+    for worksheet in sheet.worksheets():
+        if worksheet.title not in WS_S['necessary_worksheets']:
+            sheet.del_worksheet(worksheet)
+
+
+def reset_spreadsheet(sheet: gspread.Spreadsheet):
+    create_necessary_worksheets(sheet)
+    delete_unnecessary_worksheets(sheet)
+    clear_all_worksheets(sheet)
+
+
+reset_spreadsheet(sh)
