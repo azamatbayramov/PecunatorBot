edd1f45e18057a196b30c81539807eb513e18ec7
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-17 17:16:58 +0500

    Create spreadsheet.py, add basic functions

---
+++ spreadsheet.py
@@ -0,0 +1,22 @@
+import gspread
+from all_json import SETTINGS
+
+gc = gspread.service_account("credentials.json")
+
+sh = gc.open_by_url(SETTINGS["google_spreadsheet_url"])
+
+necessary_worksheets = [
+    ("Payments", 100, 10),
+    ("Users", 100, 10),
+    ("Resets", 100, 10)
+]
+
+
+def create_necessary_worksheets(sheet: gspread.Spreadsheet):
+    for worksheet in necessary_worksheets:
+        sheet.add_worksheet(*worksheet)
+
+
+def delete_all_worksheets(sheet: gspread.Spreadsheet):
+    for worksheet in sheet.worksheets():
+        sheet.del_worksheet(worksheet)
