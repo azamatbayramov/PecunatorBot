d05b500b1a2941d99b28698fcc3570c0ee9bdac3
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-04-22 15:11:26 +0500

    Add converting user and group id to str

--- utils.py
+++ utils.py
@@ -4,12 +4,12 @@ import user
 
 def get_group_and_user(message):
     g = group.Group(
-        message.chat.id,
+        str(message.chat.id),
         message.chat.title
     )
 
     u = user.User(
-        message.from_user.id,
+        str(message.from_user.id),
         message.from_user.username,
         f"{message.from_user.first_name}{' ' + message.from_user.last_name if message.from_user.last_name else ''})"
     )
