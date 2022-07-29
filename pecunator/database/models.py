864cb3cd8e78731ddb98abd2b89d897f656b0b78
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-07-29 18:57:58 +0500

    Refactor models.py

--- pecunator/database/models.py
+++ pecunator/database/models.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
 
     telegram_id = Column(Integer)
-    group_id = Column(Integer, ForeignKey("groups.id"))
+    group_id = Column(Integer, ForeignKey("groups.telegram_id"))
 
     balance = Column(Integer)
 
@@ -21,9 +21,9 @@ class User(Base):
 class Group(Base):
     __tablename__ = "groups"
 
-    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, primary_key=True)
 
-    total_balance = Column(Integer)
+    total_balance = Column(Integer, default=0)
 
     users = relationship("User")
     operations = relationship("Operation")
@@ -35,7 +35,7 @@ class Operation(Base):
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
 
     author_id = Column(Integer, ForeignKey("users.id"))
-    group_id = Column(Integer, ForeignKey("groups.id"))
+    group_id = Column(Integer, ForeignKey("groups.telegram_id"))
 
     datetime = Column(DateTime)
 
