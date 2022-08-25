6a034b86888006d406e8abb48f399c3559dbb5b7
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-08-25 19:14:44 +0300

    Add new validators

--- pecunator/utils/validators.py
+++ pecunator/utils/validators.py
@@ -24,3 +24,33 @@ def group_required(func):
         func(message)
 
     return wrapper
+
+
+def registered_group_required(func):
+    def wrapper(message):
+        telegram_id = message.chat.id
+        if not is_group(telegram_id):
+            bot.reply_to(message, "It's not a group")
+            return
+
+        if not is_registered_group(telegram_id):
+            bot.reply_to(message, "It's not a registered group")
+            return
+
+        func(message)
+
+    return wrapper
+
+
+def registered_user_required(func):
+    def wrapper(message):
+        group_id = message.chat.id
+        user_id = message.from_user.id
+
+        if not is_registered_user(group_id, user_id):
+            bot.reply_to(message, "You're not registered")
+            return
+
+        func(message)
+
+    return wrapper
